c01d3e810d7f8b4dcf2863d5cecec142fbffdc06
Mark Hahn <mark@hahnca.com>
2016-05-07 16:29:50 -0700

    fixed position

--- js/built-in-symbols.js
+++ js/built-in-symbols.js
@@ -1,22 +1,26 @@
 
 joint.shapes.logicp = {}
 
-makeLogicpSym = (name, path) => {
+makeLogicpSym = (name, path, circle) => {
   joint.shapes.logicp[name] = joint.shapes.basic.Generic.extend(
-    { markup:   '<g class="rotatable scalable"><path d="' + path + '"/></g>',
+    { markup: '<g class="rotatable scalable"><path d="' + path + '"/>' +
+      (circle ? `<circle cx="${circle.cx}" cy="${circle.cy}" r="${circle.r}"/></g>` : '</g>')
+    ,
       defaults: joint.util.deepSupplement(
         { type: 'logicp.' + name,
-           attrs: { path: { fill: 'white', stroke: 'black', 'follow-scale': true,  width: 40,  height: 40 } }
+          attrs: {
+            path: { fill: 'white', stroke: 'black', 'follow-scale': true},
+            circle: { fill: 'white', stroke: 'black', 'follow-scale': true}},
         } , joint.shapes.basic.Generic.prototype.defaults)
     });
 }
 
 makeLogicpSym(  'And',
-     'M 10 10 L 10 40 C 35 40 35 10 10 10 M 0 18 L 10 18 M 0 32 L 10 32 M 30 25 L 40 25'
-);
+  'M 10 0 V 30 C 40 30 40 0 10 0 M 0 8 L 10 8 M 0 22 L 10 22 M 33 15 L 43 15');
+
 makeLogicpSym(  'Or',
-     'M 10 10 L 10 40 C 35 40 35 10 10 10 M 0 18 L 10 18 M 0 32 L 10 32 M 30 25 L 40 25'
-);
+  'M 10 0 Q 20 15 10 30 C 40 30 40 0 10 0 M 0 8 L 13 8 M 0 22 L 13 22 M 33 15 L 43 15');
+
 makeLogicpSym(  'Not',
-     'M 10 10 L 10 40 C 35 40 35 10 10 10 M 0 18 L 10 18 M 0 32 L 10 32 M 30 25 L 40 25'
-);
+  'M 10 0 V 20 L 30 10 L 10 0 M 30 10 M 0 10 L 10 10 M 35 10 L 43 10',
+  {cx: 33, cy: 10, r:3});
--- js/editor.js
+++ js/editor.js
@@ -2,24 +2,23 @@
 var graph = new joint.dia.Graph();
 
 var paper = new joint.dia.Paper({
-    el: $('#paper'),
-    model: graph,
-    width: 200, height: 200, gridSize: 5,
+  el: $('#paper'),
+  model: graph,
+  width: 200, height: 200, gridSize: 5,
 });
 
 var and = new joint.shapes.logicp.And({
-  position: { x: 0,  y: 0 },
   size: { width: 30, height: 30 },
 });
 
 var or = new joint.shapes.logicp.Or({
-  position: { x: 0,  y: 50 },
   size: { width: 30, height: 30 },
 });
 
-var not = new joint.shapes.logicp.Not({
-  position: { x: 0,  y: 100 },
-  size: { width: 30, height: 30 },
-});
+var not = new joint.shapes.logicp.Not();
 
 graph.addCells([and, or, not]);
+
+and.position(50,50);
+or.position(50,100);
+not.position(50,150);

65d181ada428104cac4ed167c3a11ca122018645
Mark Hahn <mark@hahnca.com>
2016-05-08 21:09:28 -0700

    using devs.Model

--- js/built-in-symbols.js
+++ js/built-in-symbols.js
@@ -1,26 +1,33 @@
 
 joint.shapes.logicp = {}
 
-makeLogicpSym = (name, path, circle) => {
-  joint.shapes.logicp[name] = joint.shapes.basic.Generic.extend(
-    { markup: '<g class="rotatable scalable"><path d="' + path + '"/>' +
-      (circle ? `<circle cx="${circle.cx}" cy="${circle.cy}" r="${circle.r}"/></g>` : '</g>')
-    ,
-      defaults: joint.util.deepSupplement(
-        { type: 'logicp.' + name,
-          attrs: {
-            path: { fill: 'white', stroke: 'black', 'follow-scale': true},
-            circle: { fill: 'white', stroke: 'black', 'follow-scale': true}},
-        } , joint.shapes.basic.Generic.prototype.defaults)
-    });
+makeLogicpSym = (name, path) => {
+  let newClass = joint.shapes.logicp[name] = joint.shapes.devs.Model.extend({
+    markup: '<g class="rotatable"><g class="scalable">' +
+                    '<path class="body" d="' + path + '"/>' +
+            '</g>' +
+            '<g class="inPorts"/><g class="outPorts"/></g>',
+    portMarkup: '<g class="port port<%= id %>"><circle class="port-body"/>' +
+                 '</g>',
+  });
+  let portAttrsPortBody = newClass.__super__.defaults.attrs['.port-body'];
+  _.extend(portAttrsPortBody, {r: 3, stroke: 'green'});
 }
 
+leftCircle = (r) =>
+  ` m ${-r * 2} 0
+    a ${r} ${r} 0 1 0 ${ r * 2} 0
+    a ${r} ${r} 0 1 0 ${-r * 2} 0 `;
+
+rightCircle = (r) =>
+  ` a ${r} ${r} 0 1 0 ${ r * 2} 0
+    a ${r} ${r} 0 1 0 ${-r * 2} 0 `;
+
 makeLogicpSym(  'And',
-  'M 10 0 V 30 C 40 30 40 0 10 0 M 0 8 L 10 8 M 0 22 L 10 22 M 33 15 L 43 15');
+  'M 15 0 V 30 C 50 30 50 0 15 0 M 0 8 L 15 8 M 0 22 L 15 22' + leftCircle(3) + 'M 40 15 L 55 15');
 
-makeLogicpSym(  'Or',
-  'M 10 0 Q 20 15 10 30 C 40 30 40 0 10 0 M 0 8 L 13 8 M 0 22 L 13 22 M 33 15 L 43 15');
-
-makeLogicpSym(  'Not',
-  'M 10 0 V 20 L 30 10 L 10 0 M 30 10 M 0 10 L 10 10 M 35 10 L 43 10',
-  {cx: 33, cy: 10, r:3});
+// makeLogicpSym(  'Or',
+//   'M 10 0 Q 20 15 10 30 C 40 30 40 0 10 0 M 0 8 L 13 8 M 0 22 L 13 22 M 33 15 L 43 15');
+//
+// makeLogicpSym(  'Not',
+//   'M 10 0 V 20 L 30 10 L 10 0 M 30 10 M 0 10 L 10 10 M 35 10 L 43 10');
--- js/editor.js
+++ js/editor.js
@@ -4,21 +4,25 @@ var graph = new joint.dia.Graph();
 var paper = new joint.dia.Paper({
   el: $('#paper'),
   model: graph,
-  width: 200, height: 200, gridSize: 5,
+  // width: 200, height: 200, gridSize: 5,
 });
 
 var and = new joint.shapes.logicp.And({
-  size: { width: 30, height: 30 },
+  size: { width: 90, height: 60 },
+  inPorts: ['in1','in2'],
+  outPorts: ['out1'],
 });
 
-var or = new joint.shapes.logicp.Or({
-  size: { width: 30, height: 30 },
-});
+// var or = new joint.shapes.logicp.Or({
+//   size: { width: 30, height: 30 },
+// });
+//
+// var not = new joint.shapes.logicp.Not();
 
-var not = new joint.shapes.logicp.Not();
+graph.addCell(and);
 
-graph.addCells([and, or, not]);
-
+// graph.addCells([and, or, not]);
+//
 and.position(50,50);
-or.position(50,100);
-not.position(50,150);
+// or.position(50,100);
+// not.position(50,150);

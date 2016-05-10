34a37128b322a4469ca49b45eb26cfa9b7792c59
Mark Hahn <mark@hahnca.com>
2016-05-09 22:00:01 -0700

    started page

--- js/editor.js
+++ js/editor.js
@@ -1,18 +1,31 @@
 
+var $page = $('#page');
 var graph = new joint.dia.Graph();
-
 var paper = new joint.dia.Paper({
   el: $('#paper'),
   model: graph,
-  // width: 200, height: 200, gridSize: 5,
+  width: $page.width(), height: $page.height(),
+  gridSize: 5
 });
 
-var Or = makeLogicpSym(graph, 'Or2', 50,   0);
-var Or = makeLogicpSym(graph, 'Or3', 150,   0);
-var Or = makeLogicpSym(graph, 'Or4', 250,   0);
-var Or = makeLogicpSym(graph, 'Or5', 350,   0);
-var Or = makeLogicpSym(graph, 'Or6', 450,   0);
-var Or = makeLogicpSym(graph, 'Or7', 550,   0);
-var Or = makeLogicpSym(graph, 'Or32', 650,   0);
+makeLogicpSym(graph, 'Or2', 50,   0);
+makeLogicpSym(graph, 'Or3', 150,   0);
+makeLogicpSym(graph, 'Or4', 250,   0);
+makeLogicpSym(graph, 'Or5', 350,   0);
+makeLogicpSym(graph, 'Or6', 450,   0);
+makeLogicpSym(graph, 'Or7', 550,   0);
+makeLogicpSym(graph, 'Or16', 650,   0);
 
-var not = makeLogicpSym(graph, 'Not',  50, 100);
+makeLogicpSym(graph, 'And2', 50,   200);
+makeLogicpSym(graph, 'And3', 150,   200);
+makeLogicpSym(graph, 'And4', 250,   200);
+makeLogicpSym(graph, 'And5', 350,   200);
+makeLogicpSym(graph, 'And6', 450,   200);
+makeLogicpSym(graph, 'And7', 550,   200);
+makeLogicpSym(graph, 'And16', 650,   200);
+
+var not = makeLogicpSym(graph, 'Not',  50, 150);
+
+$(window).resize(function() {
+    paper.setDimensions($page.width(), $page.height());
+});
